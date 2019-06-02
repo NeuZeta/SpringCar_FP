7ab0b97ad4c6114a40a66c65e8ce8cd24168ea70
NeusRB <neuzeta@gmail.com>
2019-06-02 21:13:31 +0200

    WIP Home page

--- src/main/java/com/springcar/app/controllers/HomeController.java
+++ src/main/java/com/springcar/app/controllers/HomeController.java
@@ -1,24 +1,27 @@
 package com.springcar.app.controllers;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.springcar.app.models.entity.Reservation;
+import com.springcar.app.models.service.interfaces.IOfficeMasterService;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	IOfficeMasterService officeService;
+	
 	@GetMapping("/")
-	public String showHome (HttpServletRequest request, HttpServletResponse response) {		
-		HttpSession session = request.getSession(true);
+	public String showHome (HttpSession session, Model model) {		
 		Reservation rent = new Reservation();
 		session.setAttribute("reservation", rent);
+		model.addAttribute("offices", officeService.findAll());
 		return "index";
 	}
 	
